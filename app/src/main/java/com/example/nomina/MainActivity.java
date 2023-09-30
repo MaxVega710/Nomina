da35ef9a3193b2131ce4f6db01715903eef53c6f
MaxVega710 <146496965+MaxVega710@users.noreply.github.com>
2023-09-29 17:51:08 -0700

    mainActivity

--- app/src/main/java/com/example/nomina/MainActivity.java
+++ app/src/main/java/com/example/nomina/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.nomina;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       EditText txtCliente=findViewById(R.id.txtEmpleado);
+       Button btnEntrar=findViewById(R.id.btnEntrar),btnCerrar=findViewById(R.id.btnCerrar);
+
+       btnCerrar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               finish();
+           }
+       });
+
+       btnEntrar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               if (txtCliente.getText().toString().matches("")){
+                   Toast.makeText(MainActivity.this, "No ingreso un empleado", Toast.LENGTH_SHORT).show();
+               }
+               else {
+               Intent intent=new Intent(MainActivity.this, nominaActivity.class);
+               intent.putExtra("nombre",txtCliente.getText().toString());
+               startActivity(intent);
+               finish();}
+           }
+       });
+
     }
 }
